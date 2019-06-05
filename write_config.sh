eee55fc5e586f2f1007415c6e1dfe779f1e84aae
Bob Fratantonio <bobfrat@gmail.com>
2019-06-04 21:44:03 -0400

    Update config path

--- write_config.sh
+++ write_config.sh
@@ -17,7 +17,7 @@ export default {
 };
 "
     config=$(printf "$config_template" $REGION $USER_POOL_ID $APP_CLIENT_ID $IDENTITY_POOL_ID)
-    echo "$config" > ./src/config.js
+    echo "$config" > ./src/config/config.js
 }
 
 write_config
