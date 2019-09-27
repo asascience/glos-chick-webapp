4482aa665c754358f4d823f8e7daf43df01aae58
Bob Fratantonio <bobfrat@gmail.com>
2019-09-27 10:17:08 -0400

    Update alerts page design

--- src/containers/Alerts.js
+++ src/containers/Alerts.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator';
 import Table from 'react-bootstrap/Table'
 import Alert from 'react-bootstrap/Alert'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import GaugePlot from '../components/GaugePlot'
-import { TimeSeriesPlot } from '../components/TimeSeriesPlot'
+import { TimeSeriesPlot, TimeSeriesHabsPlot} from '../components/TimeSeriesPlot'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import StationMap from '../components/Map'
+import Cards from '../components/Cards'
 import MovingStats from '../components/MovingStats'
 import './StationDashboard.css';
+import '../components/Cards.scss';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import {json as requestJson} from 'd3-request';
 import { point, distance } from '@turf/turf';
 
 const DATA_URL = 'https://cors-anywhere.herokuapp.com/https://glbuoys.glos.us/static/Buoy_tool/data/meta_english.json?';
+const HABS_DATA_URL = 'https://4431mqp2sj.execute-api.us-east-2.amazonaws.com/prod/grabsample';
+
 const FEATURED_PARAM = 'BGAPCrfu';
 
 
@@ -25,8 +31,9 @@ export default class StationDashboard extends Component {
     this.state = {
       isLoading: true,
       data: null,
+      habsData: null,
       stream: [],
-      station: '',
+      station: 'tolcrib',
       tableColumns: [],
       tableData: [],
       selected: [FEATURED_PARAM],
@@ -34,10 +41,12 @@ export default class StationDashboard extends Component {
       alertMessage: ''
     };
 
+    this.blacklistParams = ['timestamp', 'date', 'station', 'topic'];
+
     this.parameterMapping = {
       cond: 'Conductivity',
-      do: 'Dissolved Oxygen Concentration',
-      odo: 'Dissolved Oxygen Concentration',
+      do: 'Dissolved Oxygen',
+      odo: 'Dissolved Oxygen',
       dosat: 'Dissolved Oxygen Saturation',
       odosat: 'Dissolved Oxygen Saturation',
       fdomQSU: 'FDOM',
@@ -59,7 +68,29 @@ export default class StationDashboard extends Component {
       BGAPCrfu: 'Blue Green Algae (rfu)',
       SpConduS: 'Specific Conductivity (uS)',
       TurbidityNTU: 'Turbidity (ntu)',
+      Temp_C: 'Sample Temp',
+      Turbidity_NTU: 'Turbidity',
+      Extracted_CHLa_ugL_1: 'Extracted Chlorophyll a',
+      Extracted_PC_ugL_1: 'Extracted Phycocyanin',
+      Particulate_Microcystin_ugL_1: 'Particulate Microcystin',
+      Dissolved_Microcystin_ugL_1: 'Dissolved Microcystin',
+      Wind_speed_knots: 'Wind Speed',
+      Secchi_Depth_m: 'Secchi Depth',
+      DO_mgL_1: 'Dissolved Oxygen',
     };
+
+    this._fetchStream();
+  }
+
+  _fetchHabs() {
+    requestJson(HABS_DATA_URL, (error, response) => {
+      if (!error) {
+        this.setState({
+          habsData: response,
+          selected: ['Dissolved_Microcystin_ugL_1']
+        });
+      }
+    });
   }
 
   _fetchStream() {
@@ -69,12 +100,23 @@ export default class StationDashboard extends Component {
       }
     });
 
-    // thisStation = 'HabsGrab';  // Hard coded for the demo
-
     const url = 'wss://gdjcxvsub6.execute-api.us-east-2.amazonaws.com/testing';
     const connection = new WebSocket(url);
-
     const movingStats = new MovingStats();
+
+    connection.onerror = e => {
+      console.error('Stream Connection Error');
+      return (
+        <div>
+          <Alert variant="danger">
+            <Alert.Heading>Error!</Alert.Heading>
+            <p>
+              Error connecting to Stream
+            </p>
+          </Alert>
+        </div>
+      )
+    }
 
     connection.onopen = e => {
       console.log('Stream opened');
@@ -89,7 +131,7 @@ export default class StationDashboard extends Component {
         return;
       }
 
-      let thisStation = this.props.match.params.id;
+      let thisStation = this.state.station;
       let filteredStream = jsonStreams.filter(function(item){
         return item.station === thisStation;
       });
@@ -120,151 +162,6 @@ export default class StationDashboard extends Component {
     }
   }
 
-  _sortStationsByDistance(data) {
-    // Start by getting the active station lat/lon
-    let thisStation = this.props.match.params.id;
-    let stationLat = null;
-    let stationLon = null;
-    let lake = null;
-    data.forEach(function(station) {
-      if (station.id === thisStation) {
-        stationLat = station.lat;
-        stationLon = station.lon;
-        lake = station.lake;
-      }
-    });
-
-    if (!stationLat) {
-      return null;
-    }
-
-    let fro = point([stationLon, stationLat]);
-    var options = {units: 'miles'};
-    // Now calculate the distance from all the other stations in nautical miles
-    data.forEach(function(station) {
-        if (station.lon && station.lat && station.obsLongName) {
-          let to = point([station.lon, station.lat]);
-          // let value = station.lake === lake ? distance(fro, to, options) : 9999.99;
-          let value = distance(fro, to, options);
-          Object.defineProperty(station, 'distance', {
-            value: value,
-            writable: true,
-            enumerable: true,
-            configurable: true
-          });
-        }
-    });
-    // Now sort by distance
-    data.sort(function(a, b) {
-      if (a.distance > b.distance) return 1;
-      if (a.distance < b.distance) return -1;
-      // a must be equal to b
-      return 0;
-    });
-    // Grab the top 5 closest stations
-    let nearestStations = data.slice(1, 6);
-    return nearestStations;
-  }
-
-  _buildTable() {
-    let self = this;
-    // Format data for react-bootstrap-table2
-    const formatWithIcon = (cell, row, rowIndex, formatExtraData) => {
-      let icon = 'arrow-circle-up';
-      if (cell === 'down') {
-        icon = 'arrow-circle-down';
-      }
-      return(
-        <span><FontAwesomeIcon icon={icon} style={{'marginRight': '7px'}}/></span>
-      )
-    }
-
-    let tableColumns = [
-      {
-        dataField: 'parameter',
-        text: 'Parameter',
-        sort: true,
-        hidden: true
-      },
-      {
-        dataField: 'prettyName',
-        text: 'Parameter',
-        sort: true,
-      },
-      // {
-      //   dataField: 'trend',
-      //   text: 'Trend',
-      //   sort: true,
-      //   formatter: formatWithIcon,
-      //   formatExtraData: formatWithIcon,
-      // }
-    ];
-    let alert = false;
-    let alertMessage = '';
-    let stream = Array.from(this.state.stream);;
-
-    if (stream.length >= 5) {
-      stream = stream.slice(0, 5);
-    }
-
-    var timestamp = 'timestamp';
-    if (!(timestamp in stream[0])) {
-      timestamp = 'date';
-      stream[0][timestamp] = Math.round(stream[0][timestamp]);
-      // jsonStream[timestamp] = Math.round(jsonStream[timestamp]/1000);
-    }
-
-    // Check the stream for the correct station
-    // let station = jsonStream.station;
-    // if (station !== thisStation) {
-    //   return null;
-    // }
-
-
-    // Check the stream for repeated record
-    // let ts = jsonStream[timestamp];
-    // let timestamps = [];
-    // this.state.stream.map((obj, idx) => {return timestamps.push(obj[timestamp])});
-    // if (ts in timestamps) {
-    //   return null;
-    // }
-
-    let featuredData = stream[0][FEATURED_PARAM];
-
-    if (featuredData > 2) {
-      alert = true;
-      alertMessage = 'This station is currently detecting Blue Green Algae values that could indicate the presence of a HAB.';
-    }
-
-    stream.map((obj, idx) => {
-      return tableColumns.push({
-        dataField: obj[timestamp].toString(),
-        text: moment.unix(obj[timestamp]).format("ddd MMM DD YYYY HH:mm:ss"),
-      });
-    });
-
-    let tableData = [];
-    let paramRows = Object.keys(stream[0]);
-    let rowObj = {};
-    paramRows = paramRows.filter(item => item !== 'timestamp' && item !== 'date' && item !== 'station' && item !== 'topic');
-    paramRows.map((param, idx) => {
-      rowObj = {
-        prettyName: param in self.parameterMapping ? self.parameterMapping[param] : param,
-        parameter: param
-      };
-      stream.map((obj, ind) => {
-        let val = obj[param];  // Take this multiplier out!
-        return rowObj[obj[timestamp]] = val.toFixed(2);
-      });
-      return tableData.push(rowObj);
-    });
-
-    return {
-      tableColumns: tableColumns,
-      tableData: tableData,
-    };
-  }
-
   async componentDidMount() {
     if (!this.props.isAuthenticated) {
       return;
@@ -276,7 +173,6 @@ export default class StationDashboard extends Component {
     }
     this.setState({ isLoading: false });
   }
-
 
   renderLander() {
     return (
@@ -303,140 +199,39 @@ export default class StationDashboard extends Component {
     )
   }
 
-  _renderNearbyStationTable(data) {
-    // const {data} = this.state;
-    if (!data) {
-      return (
-        <FontAwesomeIcon icon='spinner' size='4x' spin />
-      );
-    }
-    let nearbyStations = this._sortStationsByDistance(data);
-    if (!nearbyStations) {
-      return (
-        <FontAwesomeIcon icon='spinner' size='4x' spin />
-      );
-    }
-    let tableColumns = [
-      {
-        dataField: 'station',
-        text: 'Station',
-        sort: false,
-      },
-      {
-        dataField: 'distance',
-        text: 'Distance (nm)',
-        sort: false,
-      },
-      {
-        dataField: 'parameter',
-        text: this.parameterMapping[FEATURED_PARAM],
-        sort: false,
-      }
-    ];
-    let tableData = [];
-    nearbyStations.map((station, idx) => {
-      let ind = station.obsLongName.indexOf('Blue-Green Algae');
-      let val = ind < 0 ? 'N/A' : station.obsValues[ind];
-      let rowObj = {
-        station: station.id,
-        distance: station.distance.toFixed(2),
-        parameter: val
-      };
-      return tableData.push(rowObj);
-    });
-
-    const rowEvents = {
-      onClick: (e, row, rowIndex) => {
-      }
-    };
+  _renderMap() {
+    const {stream, data, station} = this.state;
     return (
-      <div className="dashboard-nearby-station-table">
-        <h5>Nearby Stations</h5>
-        <BootstrapTable
-          striped
-          hover
-          condensed
-          keyField='parameter'
-          data={ tableData }
-          columns={ tableColumns }
-        />
-      </div>
-    )
+        <Col sm={12}><div className='dashboard-map-container'><StationMap station={station} data={data}/></div></Col>
+      )
   }
 
-  _renderMapAndGauge() {
-    const {stream, data} = this.state;
-    let thisStation = this.props.match.params.id;
-    if (stream.length === 0 || !data) {
+  _renderPlots(habsData) {
+    const {stream, data, station} = this.state;
+    if ( (stream.length === 0 || !data) && !habsData) {
       return null;
     }
 
-    // Add station to data
-    // let ids = data.map(a => a.id);
-    // if (ids.includes("HabsGrab")) {
-    //   for (let i in data) {
-    //      if (data[i].id === 'HabsGrab') {
-    //         console.log('HABS GRAB')
-    //         // data[i].lat = Math.random() + 41;
-    //         // data[i].lon = Math.random() + -71;
-    //         data[i].lat = stream[0].lat + Math.random() * 15;
-    //         data[i].lon = stream[0].lon + Math.random() * 15;
-    //         console.log(data[i].lat)
-    //         console.log(data[i].lon)
-    //         break; //Stop this loop, we found it!
-    //      }
-    //    }
-    // } else {
-      let newData = JSON.parse(JSON.stringify(data));
-      let obsLongName = Object.keys(stream[0]);
-      obsLongName = obsLongName.filter(item => item !== 'timestamp' && item !== 'date' && item !== 'station' && item !== 'topic');
-      let obsValues = [];
-      let obsUnits = [];
-      obsLongName.forEach(function (item, index) {
-        obsValues.push(stream[0][item]);
-        obsUnits.push('');
-      });
-      newData.push({
-          "lon": stream[0].lon,
-          "recovered": false,
-          "lat": stream[0].lat,
-          "timeZone": "America/New_York",
-          "buoyInfo": "This buoy is for demo purposes only",
-          "buoyAlert": "",
-          "id": "HabsGrab",
-          "lake": "ER",
-          "longName": "Habs Grab",
-          "obsID": [],
-          "obsUnits": obsUnits,
-          "obsLongName": obsLongName,
-          "updateTime": "2019-05-28T16:30:00Z",
-          "obsValues": obsValues
-      });
-    // }
-
-    return (
-      <div>
-        <Container>
-          <Row>
-            <Col sm={5}><div className='dashboard-map-container'><StationMap station={thisStation} data={newData}/></div></Col>
-            <Col sm={4}>{this._renderNearbyStationTable(newData)}</Col>
-            <Col sm={3}><GaugePlot stream={stream} parameter={FEATURED_PARAM} parameterMapping={this.parameterMapping}/></Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-
-  _renderTimeSeriesPlot() {
-    const {stream, selected} = this.state;
-    if (stream.length === 0) {
-      return null;
-    }
+    // Replace with selected
+    let params = ['BGAPCrfu', 'ysiturbntu', 'BGAPCrfu'];
     const colors = ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"];
     return (
       <div>
-        {selected.map((param, idx) => {
-          return <TimeSeriesPlot key={param} stream={stream} parameters={[param]} parameterMapping={this.parameterMapping} color={colors[idx % colors.length]}/>
+        {params.map((param, idx) => {
+          let dataPoint, timestamp;
+          if (habsData) {
+            let vals = habsData.properties.data[param].values;
+            dataPoint = vals[vals.length - 1];
+          } else {
+            let timeParam = 'timestamp' in stream[0] ? 'timestamp' : 'date';
+            dataPoint = stream[0][param];
+          }
+          return (
+            <Row>
+              <Col sm={3}><GaugePlot dataPoint={dataPoint} parameter={this.parameterMapping[param]}/></Col>
+              <Col sm={9}><TimeSeriesPlot key={param} stream={stream} parameters={[param]} parameterMapping={this.parameterMapping} color={colors[idx % colors.length]}/></Col>
+            </Row>
+          )
         })}
       </div>
     );
@@ -467,37 +262,15 @@ export default class StationDashboard extends Component {
     }
   }
 
-  _renderTable() {
-    const {tableData, tableColumns} = this._buildTable();
-
-    if (tableData.length === 0) {
-      return null;
-    }
-    const selectRow = {
-      mode: 'checkbox',
-      clickToSelect: true,
-      selected: this.state.selected,
-      onSelect: this.handleOnSelect,
-      onSelectAll: this.handleOnSelectAll,
-    };
-
-    return (
-      <div className="container" style={{ marginTop: 50 }}>
-        <BootstrapTable
-          striped
-          hover
-          keyField='parameter'
-          data={ tableData }
-          columns={ tableColumns }
-          selectRow={ selectRow }
-        />
-      </div>
-    );
+  handleClick(selected){
+    this.setState({
+      selected: selected
+    });
   }
 
   renderDashboard() {
     const {stream, station} = this.state;
-    let thisStation = this.props.match.params.id;
+    let thisStation = 'tolcrib';
     if (station !== thisStation) {
       this._fetchStream();
       this.setState({
@@ -511,17 +284,32 @@ export default class StationDashboard extends Component {
       )
     }
     let stationName = this.state.station;
+    let lastUpdate = moment.unix(this.state.stream[0]['timestamp']).format("ddd MMM DD YYYY hh:mm a");
+
     return (
       <div className="home-container">
         {this._renderAlert()}
-        <h1 align='center'>Station {stationName}</h1>
-        <div id="plot">
-          {this._renderMapAndGauge()}
-          {this._renderTimeSeriesPlot()}
-        </div>
-        <div id="table">
-          {this._renderTable()}
-        </div>
+        <h1 align='center'>Alerts</h1>
+        <h2 align='left'>Station - {stationName}</h2>
+        <h5 align='left'>Last Updated - {lastUpdate}</h5>
+        <Row>
+          <Col sm={6}>
+            <div>
+            </div>
+          </Col>
+          <Col sm={6}>
+            <div>
+              {this._renderMap()}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <div id="plot" style={{marginBottom: '100px'}}>
+              {this._renderPlots()}
+            </div>
+          </Col>
+        </Row>
       </div>
     )
   }
