923000515af6de92d89f1f8e4538864b37e5a0bf
Bob Fratantonio <bobfrat@gmail.com>
2019-10-03 13:55:49 -0400

    Update home page and add logos

---
+++ src/components/Logos.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import glosLogo from '../logos/GLOSLogo_and_Name_horizontal.png';
+import glerlLogo from '../logos/US-GreatLakesEnvironmentalResearchLaboratory.png';
+import ciglrLogo from '../logos/CIGLR-LOGO.png';
+import osuLogo from '../logos/OSU_Stacked_RedBlack.png';
+import cwaLogo from '../logos/Cleveland_Water_Alliance_CWA-logo.png';
+import nccosLogo from '../logos/NCCOSLogo.png';
+import rpsLogo from '../logos/rps.jpg';
+import limnoLogo from '../logos/LimnoTech_Logo.png';
+import './Logos.css';
+
+export default class Logos extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => {
+    let logos = [];
+    return (
+      <Container className="logos">
+        <Row style={{paddingTop: '20px'}}>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={glosLogo} alt="glosLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={glerlLogo} alt="glerlLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={ciglrLogo} alt="ciglrLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={osuLogo} alt="osuLogo"/>
+          </Col>
+        </Row>
+        <Row style={{paddingTop: '20px'}}>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={cwaLogo} alt="cwaLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={nccosLogo} alt="nccosLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={rpsLogo} alt="RPSLogo"/>
+          </Col>
+          <Col lg={3} sm={6} xs={12}>
+            <img src={limnoLogo} alt="limnoLogo"/>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer, Link } from 'react-router-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import StationMap from '../components/Map';
+import Logos from '../components/Logos';
 import './Home.css';
 
 
@@ -34,25 +35,7 @@ export default class Home extends Component {
         <div className='jumbotron jumbotron-fluid'>
           <div className="container">
             <h1>Lake Erie Early Warning System</h1>
-            <h1>Prototype</h1>
-            <h4 align='left' style={{paddingTop: '20px'}}>Welcome!</h4>
-            <p align='left'>This app is a way to provide the latest information on western Lake Erie harmful algal blooms to stakeholders of all kinds,We
-               want to make sure this critical information needs to be accessible whether people are using a mobile device or sitting at their
-               desktop.</p>
-            <p align='left'>Thank you for helping us as we develop what we hope will be a valuable tool for those making critical water treatment
-               decisions.</p>
-
-            <p align='left'>At this phase of development, the app shows information taken from 1) sensors mounted on buoys and water intakes, 2)
-               toxicity measurements from field samples, 3) the NOAA Great Lakes Environmental Research Laboratory (GLERL) Experimental
-               Lake Erie HAB Tracker forecast model, and 4) another forecast model called the GLERL Great Lakes Coastal Forecasting
-               System (GLCFS) that predicts water current direction and speed.</p>
-
-            <p align='left'>As this project expands, we will include more data on both HABs and hypoxia.</p>
-
-            <p align='left'>Please explore the prototype, and, as always, let us know your thoughts using the widget in the lower right hand corner. Your
-            feedback is invaluable. </p>
-
-            <p align='left'>Read more on the project and the products that provide the data here.</p>
+            <h2>Prototype</h2>
             <Container>
               <Row style={{paddingTop: '20px'}}>
                 <Col sm={3}/>
@@ -69,6 +52,21 @@ export default class Home extends Component {
                 <Col sm={3}/>
               </Row>
             </Container>
+            <h4 align='left' style={{paddingTop: '20px'}}>Welcome!</h4>
+            <p align='left'>The Great Lakes Observing System and our partners have developed this web application prototype to provide the latest
+              information about western Lake Erie harmful algal blooms to stakeholders of all kinds.</p>
+            <p align='left'>The application’s goal is to provide actionable HAB information, when and where people need it so they can spend less
+              time looking for information and more time putting it to use.</p>
+
+            <p align='left'><i>Sign up and get started!</i></p>
+
+            <p align='left'>Learn more about the project <a href='/project'>here</a></p>
+
+            <p align='left'>Learn more about the data <a href='/data'>here</a></p>
+
+            <p align='left'>The Team</p>
+            <Logos/>
+
           </div>
         </div>
       </div>
