cc0e8487f75694b24b6eb4c835711bf6edb81ebd
christensenmichael0 <christensenmichael0@gmail.com>
2020-05-05 16:53:36 -0400

    Work on fixing authentication issue when navigating directly to a station.

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
@@ -10,16 +10,44 @@ import Project from './containers/Project';
 import Data from './containers/Data';
 import StationDashboard from './containers/StationDashboard'
 
-export default ({ childProps }) => (
-    <Switch>
-        <AppliedRoute path="/" exact component={Home} props={childProps} />
-        <AppliedRoute path="/login" exact component={Login} props={childProps} />
-        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-        <AppliedRoute path="/alerts" exact component={Alerts} props={childProps} />
-        <AppliedRoute path="/project" exact component={Project} props={childProps} />
-        <AppliedRoute path="/data" exact component={Data} props={childProps} />
-        <Route path="/:id" component={StationDashboard} />
-        {/* Finally, catch all unmatched routes */}
-        <Route component={NotFound} />
-    </Switch>
-);
+function PrivateRoute({ children, ...rest }) {
+
+    const {isAuthenticated} = rest;
+
+    return (
+        <Route
+            {...rest}
+            render={({ location, match }) =>
+                isAuthenticated ? (
+                        React.cloneElement(children,{...rest, match})
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+export default ({ childProps }) => {
+    return (
+        <Switch>
+            <AppliedRoute path="/" exact component={Home} props={childProps}/>
+            <AppliedRoute path="/login" exact component={Login} props={childProps}/>
+            <AppliedRoute path="/signup" exact component={Signup} props={childProps}/>
+            <AppliedRoute path="/alerts" exact component={Alerts} props={childProps}/>
+            <AppliedRoute path="/project" exact component={Project} props={childProps}/>
+            <AppliedRoute path="/data" exact component={Data} props={childProps}/>
+            <PrivateRoute path="/:id" {...childProps}>
+                <StationDashboard/>
+            </PrivateRoute>
+            {/*<AppliedRoute path="/:id" component={StationDashboard} props={childProps}/>*/}
+            {/* Finally, catch all unmatched routes */}
+            <Route component={NotFound}/>
+        </Switch>
+    )
+};
